e0fe7f45bb65294a46de667288780d9eb04b03da
Máximo Liberata Torres <maximoliberata0@gmail.com>
2024-04-16 15:05:33 -0400

    feat: remove extra code from Babel file config

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,6 @@ module.exports = function (api) {
 		presets: ['babel-preset-expo'],
 		plugins: [
 			'react-native-reanimated/plugin',
-			['@babel/plugin-transform-private-methods', { loose: true }],
-			['@babel/plugin-transform-private-property-in-object', { loose: true }],
 			[
 				'module-resolver',
 				{
