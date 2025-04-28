342992e7a63ca9356ef2f5f1a3312029f71fa4b0
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-04-28 10:48:05 -0400

    feat: change mqtt import

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enablePackageExports = true;
+
+module.exports = config;
